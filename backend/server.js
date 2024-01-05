4a0f72b66592c84a3ee4f640468025fceb580706
sumonDev13 <sumonmondal1530765@gmail.com>
2024-01-06 02:06:41 +0600

    mongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import connectDB from './db/dbConnect.js';
+
+dotenv.config();
+connectDB();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cookieParser());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({extended: true}));
+
+app.listen(port,()=>{
+    console.log(`app listening on ${port}`);
+})
