e0aeaabb1d2bc49408c0b4e207e6cb6cb3963991
sumonDev13 <sumonmondal1530765@gmail.com>
2024-01-05 20:19:21 +0600

    routing

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -1,10 +1,14 @@
 import MenuDropdown from "../menuDropdown/MenuDropdown";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className="bg-gray-800 p-4">
       <div className="flex items-center justify-between">
+        <Link to='/'>
         <h1 className="text-white text-2xl">SumonDev13</h1>
+        </Link>
+        
         <div className="ml-auto">
           <MenuDropdown />
         </div>
