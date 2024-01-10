b014045a319f8a471558def8a19497bb1d88618b
sumonDev13 <sumonmondal1530765@gmail.com>
2024-01-10 14:36:30 +0600

    api running

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,8 @@ app.use(cookieParser());
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended: true}));
 
-app.get('/', (req, res) =>{
-    res.json('Backend successfully running');
+app.use('/', (req, res) =>{
+    res.json({message:'Backend successfully running api'});
 });
 
 app.listen(port,()=>{
